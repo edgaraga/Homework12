368cda04f7d4087efa3c0e6dfe5feb3f4a585e28
Агабекян Эдгар Артемович <agabekyan.e@milandr.ru>
2022-06-10 16:11:29 +0500

    Product manager rich model

--- src/main/java/ProductRepository.java
+++ src/main/java/ProductRepository.java
@@ -1,17 +1,6 @@
 public class ProductRepository {
 
     private Product[] items = new Product[0];
-    /*private Product[] items = {
-            new Product(1, "Отцы и дети", 800, "Тургенев"),
-            new Product(2, "iPhone SE", 50000, "Apple"),
-            new Product(3, "Война и Мир", 5000, "Толстой"),
-            new Product(4, "Galaxy S 10", 60000, "Samsung"),
-            new Product(5, "Преступление и наказание", 3000, "Достоевский"),
-            new Product(6, "Mi Not 10 Light", 30000, "Xiaomi"),
-            //new ViewingItem(707, "Номер один")
-    };
-
-     */
 
     public void save(Product item) {
         int length = items.length + 1;
